a6ece93497a4cdf6ffa3479b1a2dc364a2e3369a
Leo Pho <Leo@Leos-MBP.home>
2019-09-16 22:35:53 -0400

    added code for module 6

---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', MsgController);
+
+  MsgController.$inject = ['$scope'];
+  function MsgController($scope) {
+    $scope.message = "";
+    $scope.items = "";
+
+    $scope.check = function () {
+      var itemList = ($scope.items).split(',') || [];
+      itemList = itemList.filter(function(n){ return n });
+
+      if (itemList.length === 0) {
+        $scope.message = "Please enter data first!";
+        $scope.style = "red";
+      } else if (itemList.length <= 3) {
+        $scope.message = "Enjoy!";
+        $scope.style = "green";
+      } else {
+        $scope.message = "Too much!";
+        $scope.style = "green";
+      }
+    };
+  }
+})();
