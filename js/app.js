03878bd2f4c582acd96d1563a4ccee95994b9572
Leo Pho <Leo@Leos-MBP.home>
2019-09-18 20:33:31 -0400

    updated module 6

--- js/app.js
+++ js/app.js
@@ -2,21 +2,27 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', MsgController);
+.controller('LunchCheckController', LunchCheckController);
 
-  MsgController.$inject = ['$scope'];
-  function MsgController($scope) {
+LunchCheckController.$inject = ['$scope'];
+  
+function LunchCheckController($scope) {
     $scope.message = "";
     $scope.items = "";
 
     $scope.check = function () {
       var itemList = ($scope.items).split(',') || [];
-      itemList = itemList.filter(x => x);
-
-      if (itemList.length === 0) {
+      var count = 0;
+      for(var i in itemList){
+        var item = itemList[i].trim();
+        if(item !== ""){
+          count++;
+        }
+      }
+      if (count === 0) {
         $scope.message = "Please enter data first!";
         $scope.style = "red";
-      } else if (itemList.length <= 3) {
+      } else if (count <= 3) {
         $scope.message = "Enjoy!";
         $scope.style = "green";
       } else {
