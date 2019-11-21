b1086d28d740214319ebdb92e93a73665a15af5f
Leo Pho <Leo@Leos-MBP.home>
2019-11-20 21:10:04 -0500

    clean up

--- items/items.controller.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	'use strict';
-	angular.module('MenuApp').controller('ItemsController', ItemsController);
-	ItemsController.$inject = ['items'];
-	function ItemsController ( categoryItems) {
-		this.categoryItems = categoryItems.menu_items;
-	};
-}());
